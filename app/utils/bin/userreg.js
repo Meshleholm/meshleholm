5d9a873bf1c867e3c56a4613a008463396d0e51d
willeponken <willeponken@gmail.com>
2014-06-13 14:30:17 +0200

    Added conditions and warranty

--- app/utils/bin/userreg.js
+++ app/utils/bin/userreg.js
@@ -245,11 +245,17 @@ function CommandSwitch(cmd, callback) {
       });
       break;
     case 'warranty':
-      console.log('"THE LICENCE"\n');
+      console.log('This program is distributed in the hope that it will be useful,\n' +
+      'but WITHOUT ANY WARRANTY; without even the implied warranty of\n' +
+      'MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\n' +
+      'GNU General Public License for more details.\n');
       callback();
       break;
     case 'conditions':
-      console.log('"CONDITIONS"\n');
+      console.log('This program is free software: you can redistribute it and/or modify\n' +
+      'it under the terms of the GNU General Public License as published by\n' +
+      'the Free Software Foundation, either version 3 of the License, or\n' +
+      '(at your option) any later version.\n');
       callback();
       break;
     case 'help':
